b80cd5a3e548bcd1bde7202c2bb03b7e6aad6f3e
Tais <649748661@qq.com>
2019-11-03 20:55:57 +0800

    new

--- googleSecret.py
+++ googleSecret.py
@@ -12,8 +12,27 @@ def get_google_secret_code(secret):
     key = base64.b32decode(secret, True)
     msg = struct.pack(">Q", intervals_no)
     h = hmac.new(key, msg, hashlib.sha1).digest()
-    o = ord(chr(h[19])) & 15 
-    #o = ord(h[19]) & 15 # used python 2
+    o = ord(chr(h[19])) & 15
+    # o = ord(h[19]) & 15 # used python 2
     h = (struct.unpack(">I", h[o:o+4])[0] & 0x7fffffff) % 1000000
     code = str(h).zfill(6)
     return code
+
+
+def continued_print_google_code(secret):
+    while True:
+        google_code = get_google_secret_code(secret)
+        print("google_code : %s" % google_code)
+        while True:
+            if google_code == get_google_secret_code(secret):
+                if time.gmtime(time.time()).tm_sec % 30 > 25:
+                    print("google code will be invalid ,countdown %d s" % (30 - time.gmtime(time.time()).tm_sec % 30))
+                    time.sleep(1)
+                else:
+                    time.sleep(2)
+            else:
+                break
+
+
+if __name__ == '__main__':
+    continued_print_google_code(secret="BQ6KK6M5SUB44IJ5")
