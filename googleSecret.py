df580e7ead0973c97bbc47ffb25a80df4f7b4bf9
Tais <649748661@qq.com>
2019-11-03 20:16:48 +0800

    test

--- googleSecret.py
+++ googleSecret.py
@@ -1,13 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-
 import hmac, base64, struct, hashlib, time
-import sys
-sys.path.append('../')
 
 
-def get_f2a_code(secret):
+def get_google_secret_code(secret):
     intervals_no = int(time.time())//30
     lens = len(secret)
     lenx = 8 - (8 if lens % 8 == 0 else lens % 8)
@@ -15,7 +12,8 @@ def get_f2a_code(secret):
     key = base64.b32decode(secret, True)
     msg = struct.pack(">Q", intervals_no)
     h = hmac.new(key, msg, hashlib.sha1).digest()
-    o = ord(chr(h[19])) & 15
+    o = ord(chr(h[19])) & 15 
+    #o = ord(h[19]) & 15 # used python 2
     h = (struct.unpack(">I", h[o:o+4])[0] & 0x7fffffff) % 1000000
     code = str(h).zfill(6)
     return code
